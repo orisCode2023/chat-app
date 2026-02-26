c033d505d00906934d0a1d38a4a9ed7a9b358df7
odmark2023@gmail.com <odmark2023@gmail.com>
2026-02-26 11:43:26 +0200

    creating new user and crypt is password and insert to db

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,49 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
     try {
-        const {fullName, username, password, confirmPassword, gender} = req.body
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Password dont match" });
+        }
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "user already exist" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePick: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePick: newUser.profilePick,
+        });
+
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: 'Internal server Error' })
     }
 };
 export const login = (req, res) => {
-    res.send("hi login")
+    res.send("hi login");
 };
 export const logout = (req, res) => {
-    res.send("hi out")
+    res.send("hi out");
 };
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'
+import connectDb from './db/dbConfig.js';
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('server ready');
-})
+dotenv.config();
 
+app.use(express.json())
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
+    connectDb();
     console.log(`server running on port ${PORT} `);
 });
